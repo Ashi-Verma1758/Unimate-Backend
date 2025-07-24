0d271576199381175250c232ef7184b20ac43790
yashi-gupta18 <yashiiigupta18@gmail.com>
2025-07-24 23:32:02 +0530

    invites

--- src/controllers/invite.controller.js
+++ src/controllers/invite.controller.js
@@ -151,3 +151,19 @@ export const getSentRequests = asyncHandler(async (req, res) => {
 
     res.status(200).json(new ApiResponse(200, sentRequests));
 });
+ function calculateTimeAgo(date) {
+    const now = new Date();
+    const seconds = Math.floor((now - new Date(date)) / 1000);
+
+    let interval = seconds / 31536000; // years
+    if (interval > 1) return Math.floor(interval) + " years ago";
+    interval = seconds / 2592000; // months
+    if (interval > 1) return Math.floor(interval) + " months ago";
+    interval = seconds / 86400; // days
+    if (interval > 1) return Math.floor(interval) + " days ago";
+    interval = seconds / 3600; // hours
+    if (interval > 1) return Math.floor(interval) + " hours ago";
+    interval = seconds / 60; // minutes
+    if (interval > 1) return Math.floor(interval) + " minutes ago";
+    return Math.floor(seconds) + " seconds ago";
+}
